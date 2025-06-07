c26447ce13d3e98877db9a039a01698e43ca1d3d
Matthew Crossley <mq20185996@Matthews-MacBook-Air.local>
2025-06-07 13:12:24 +1000

    small analysis mods

--- projects/cat_buttons_vs_reaches/code/inspect_results.py
+++ projects/cat_buttons_vs_reaches/code/inspect_results.py
@@ -14,20 +14,22 @@ d_rec = []
 for f in os.listdir(dir_data):
     if f.endswith("data.csv"):  # NOTE: exludes "move" files
         d = pd.read_csv(os.path.join(dir_data, f))
-        d.sort_values(by=["condition", "subject", "trial"], inplace=True)
-        d["acc"] = d["cat"] == d["resp"]
-        d["cat"] = d["cat"].astype("category")
-        d["trial"] += 1
-        dd = d[["trial", "condition", "acc"]].copy()
-        dd["intercept"] = 1
-        endog = dd["acc"]
-        exog = patsy.dmatrix("np.log(trial)",
-                             data=dd,
-                             return_type="dataframe")
-        model = sm.GLM(endog, exog, family=sm.families.Binomial())
-        fm = model.fit()
-        d["pred"] = fm.predict(exog)
-        d_rec.append(d)
+        if d.shape[0] == 400:
+            print(d.shape)
+            d.sort_values(by=["condition", "subject", "trial"], inplace=True)
+            d["acc"] = d["cat"] == d["resp"]
+            d["cat"] = d["cat"].astype("category")
+            d["trial"] += 1
+            dd = d[["trial", "condition", "acc"]].copy()
+            dd["intercept"] = 1
+            endog = dd["acc"]
+            exog = patsy.dmatrix("np.log(trial)",
+                                 data=dd,
+                                 return_type="dataframe")
+            model = sm.GLM(endog, exog, family=sm.families.Binomial())
+            fm = model.fit()
+            d["pred"] = fm.predict(exog)
+            d_rec.append(d)
 
 d = pd.concat(d_rec)
 
@@ -60,13 +62,24 @@ print(d.groupby(["condition"])["subject"].nunique())
 #     # ds.plot(subplots=True)
 #     # plt.show()
 
-fig, ax = plt.subplots(d["subject"].unique().shape[0], 1, squeeze=False, figsize=(12, 8))
-ax = ax.flatten()
-for i, s in enumerate(d["subject"].unique()):
-    axx = ax[i]
-    ds = d[d["subject"] == s]
-    sns.lineplot(data=ds, x="trial", y="acc", ax=axx)
-    sns.lineplot(data=ds, x="trial", y="pred", ax=axx)
-    axx.set_title(f"Subject {s} - {ds.condition.unique()[0]}")
+dd = d.groupby(["condition", "trial"]).agg(
+    acc=("acc", "mean"),
+    pred=("pred", "mean")
+).reset_index()
+
+fig, ax = plt.subplots(4, 1, squeeze=False, figsize=(8, 10))
+for i, c in enumerate(dd.condition.unique()):
+    axx = ax[i, 0]
+    ddc = dd[dd["condition"] == c]
+    sns.lineplot(data=ddc,
+                 x="trial",
+                 y="acc",
+                 ax=axx)
+    sns.lineplot(data=ddc,
+                 x="trial",
+                 y="pred",
+                 ax=axx)
+    axx.set_title(c)
+    axx.set_ylim(0.2, 1)
 plt.tight_layout()
 plt.show()
